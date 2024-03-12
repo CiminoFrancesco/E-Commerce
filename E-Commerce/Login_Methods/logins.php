053f02b7b515941c009e11ca2610df83d6b0ccae
CiminoFrancesco <145446693+CiminoFrancesco@users.noreply.github.com>
2024-03-12 12:57:08 +0100

    Add files via upload

---
+++ E-Commerce/Login_Methods/logins.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> Registrati e Entra</title>
+    <link rel="stylesheet" type="text/css" href="../resources/stylesheet.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script>
+        function validateForm() {
+            var password = document.getElementById("password").value;
+            var confirmPassword = document.getElementById("confirmPassword").value;
+
+            if (password !== confirmPassword) {
+                document.getElementById("password-error").innerHTML = "Le password non corrispondono.";
+                return false;
+            } else {
+                document.getElementById("password-error").innerHTML = "";
+                return true;
+            }
+        }
+    </script>
+</head>
+
+<body>
+
+    <?php
+    session_start();
+
+    if (isset($_SESSION['ErroreLogin']) && $_SESSION['ErroreLogin'] == true) {
+    ?>
+        <script>
+            window.alert("Credenziali Errate!");
+        </script>
+    <?php
+    }
+
+    ?>
+
+    <div class="container" id="container">
+        <div class="form-container sign-up-container">
+            <form action="registrazione.php" id="registrationForm" method="post" onsubmit="return validateForm()">
+                <h2 style="padding-top: 15px"> Crea un Utente</h2>
+                <input type="email" id="Email" name="Email" placeholder="Email" required>
+                <input type="text" name="username" placeholder="Username" required>
+                <input type="text" name="cognome" placeholder="Cognome" required>
+                <input type="password" id="password" name="password" placeholder="Password" required>
+                <input type="password" id="confirmPassword" placeholder="Conferma Password" naeme="confirmPassword" required>
+                <div id="password-error"></div>
+                <button type="submit" style="margin-top:10px"> Crea Utente</button>
+                <h5>Sono gi&agrave; registrato <a class="ghost" id="signIn" style="color:blue;"><u>Entra!</u></a></h5>
+            </form>
+        </div>
+        <div class="form-container sign-in-container">
+            <form action="login.php" method="post" onsubmit="return validateForm()">
+                <h2 style="padding-bottom: 15px"> Entra</h2>
+                <input type="text" name="username" placeholder="Username">
+                <input type="password" name="password" placeholder="Password">
+                <button style="margin-top: 10px"> Entra...</button>
+                <h5><a class="ghost" id="signUp" style="color:blue;"><u>Registrati!</u></a></h5>
+            </form>
+
+        </div>
+        <div class="overlay-container">
+            <div class="overlay">
+                <div class="overlay-panel overlay-left">
+                    <img src="../resources/images/img1.PNG" alt="movie-1" height="480" width="500" />
+
+                </div>
+                <div class="overlay-panel overlay-right">
+                    <img src="../resources/images/img2.PNG" alt="movie-2" height="480" width="500" />
+                </div>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+        const signUpButton = document.getElementById('signUp');
+        const signInButton = document.getElementById('signIn');
+        const container = document.getElementById('container');
+
+        signUpButton.addEventListener('click', () => {
+            container.classList.add("right-panel-active");
+        });
+        signInButton.addEventListener('click', () => {
+            container.classList.remove("right-panel-active");
+        });
+    </script>
+</body>
+
+</html>
---
+++ E-Commerce/SQL/Query_E-Commerce.sql
@@ -0,0 +1,78 @@
+-- Creazione Database 
+CREATE DATABASE IF NOT EXISTS e_commerce;
+USE e_commerce;
+
+-- Creazione Tabelle
+CREATE TABLE IF NOT EXISTS cliente (
+    mail VARCHAR(255) PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    cognome VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS categoria (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    descrizione VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS prodotto (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    descrizione VARCHAR(255) NOT NULL,
+    prezzo DECIMAL(10, 2) NOT NULL,
+    -- Precision (10) è il numero massimo di caratteri che può avere / Scale (2) è il numero di cifre decimali massimo che vengono visualizzate
+    ID_Categoria INT NOT NULL,
+    img_path VARCHAR(255) NOT NULL,
+    FOREIGN KEY (ID_Categoria) REFERENCES categoria(ID)
+);
+
+CREATE TABLE IF NOT EXISTS carrello (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    mail_Cliente VARCHAR(255) NOT NULL,
+    
+    FOREIGN KEY (mail_Cliente) REFERENCES cliente(mail)
+);
+
+CREATE TABLE IF NOT EXISTS ordine (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    Data_Ordine DATE NOT NULL, 
+    indirizzo VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS dettaglio_ordine (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    ID_Ordine INT NOT NULL,
+    
+    FOREIGN KEY (ID_Ordine) REFERENCES ordine(ID)
+);
+
+CREATE TABLE if NOT EXISTS prodotti_Carrello (
+	ID INT PRIMARY KEY AUTO_INCREMENT,
+	Mail_Cliente VARCHAR(255) NOT NULL,
+	ID_Carrello INT NOT NULL,
+	ID_Prodotto INT NOT NULL,
+	
+	FOREIGN KEY (ID_Carrello) REFERENCES carrello(ID),
+	FOREIGN KEY (Mail_Cliente) REFERENCES cliente(mail),
+	FOREIGN KEY (ID_Prodotto) REFERENCES prodotto(ID)
+);
+
+
+-- Inserimento dati nelle tabelle delle categorie
+
+INSERT INTO categoria (nome, descrizione)
+VALUES 
+    ('Mobili da Cucina', 'Mobili e attrezzi per la cucina'),
+    ('Arredamento per il Salotto', 'Mobili e accessori per il salotto'),
+    ('Elettronica', 'Prodotti elettronici per la casa');
+
+-- Inserimento dati nelle tabelle dei prodotti
+INSERT INTO prodotto (nome, descrizione, prezzo, ID_Categoria, img_path)
+VALUES
+    ('Set di Coltelli da Cucina', 'Coltelli di alta qualità per la cucina', 49.99, 1, '../resources/images/1.jpg'),
+    ('Bilancia Digitale', 'Bilancia digitale precisa per la cucina', 29.99, 1, '../resources/images/2.jpg'),
+    ('Divano Moderno', 'Divano moderno e confortevole', 399.99, 2, '../resources/images/3.jpg'),
+    ('Tavolino da Salotto in Vetro', 'Tavolino elegante per il tuo salotto', 59.99, 2, '../resources/images/4.jpg'),
+    ('Smart Speaker', 'Altoparlante intelligente con assistente virtuale', 79.99, 3, '../resources/images/5.jpg'),
+    ('Lampada Intelligente', 'Lampada con controllo intelligente', 29.99, 3, '../resources/images/6.jpg');
